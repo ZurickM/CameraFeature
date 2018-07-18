2dd5ef048c6301494adaa348378eb62bd4996e4f
Cap'n_Rob <robb1772@hotmail.com>
2018-07-17 23:08:00 -0700

    camera fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,41 +2,21 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { CameraPreview } from '@ionic-native/camera-preview';
+
 import { HomePage } from '../pages/home/home';
  
 @Component({
   template: `<ion-nav [root]="rootPage"></ion-nav>`
 })
 export class MyApp {
-  rootPage = HomePage;
- 
-  constructor(platform: Platform, private splashScreen: SplashScreen, private statusBar: StatusBar) {
+  rootPage:any = HomePage;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
- 
-      StatusBar.styleDefault();
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
       splashScreen.hide();
- 
-      let tapEnabled: any = false;
-      let dragEnabled: any = false;
-      let toBack: any = true;
-      let alpha = 1;
-      let rect: any = {
-        x: 0,
-        y: 0,
-        width: platform.width(),
-        height: platform.height()
-      };
- 
-      CameraPreview.startCamera(
-        rect,
-        'rear',
-        tapEnabled,
-        dragEnabled,
-        toBack,
-        alpha
-      );
- 
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { CameraPreview } from '@ionic-native/camera-preview';
 @NgModule({
   declarations: [
     MyApp,
@@ -24,6 +24,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    CameraPreview,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
